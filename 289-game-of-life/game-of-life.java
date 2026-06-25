373c48192b2666e410801a1a3d0293f17a0e0e16
Jha Aryan <aryan42006@gmail.com>
2026-06-25 14:47:10 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (79.34%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -6,8 +6,7 @@ class Solution {
 
         int[][] copy = new int[m][n];
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++)
-                copy[i][j] = board[i][j];
+            for (int j = 0; j < n; j++) copy[i][j] = board[i][j];
         }
 
         for (int i = 0; i < m; i++) {
@@ -18,24 +17,17 @@ class Solution {
                     for (int c = j - 1; c <= j + 1; c++) {
 
                         if (r >= 0 && r < m && c >= 0 && c < n) {
-                            if (r == i && c == j)
-                                continue;
-
+                            if (r == i && c == j) continue;
                             live += copy[r][c];
                         }
                     }
                 }
-
                 if (copy[i][j] == 1) {
 
                     if (live < 2) board[i][j] = 0;
                     else if (live == 2 || live == 3) board[i][j] = 1;
                     else board[i][j] = 0;
-
-                } else {
-                    if (live == 3) board[i][j] = 1;
-                    else board[i][j] = 0;
-                }
+                } else  if (live == 3) board[i][j] = 1;
 
             }
         }
