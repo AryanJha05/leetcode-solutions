ef6eeec8f9f418cfd695880cccab97d84cffe926
Jha Aryan <aryan42006@gmail.com>
2026-07-06 13:41:14 +0530

    Time: 1 ms (90.78%) | Memory: 43.2 MB (72.62%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,13 +1,24 @@
+// class Solution {
+//     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        
+//         StringBuilder newWord1 = new StringBuilder();
+//         StringBuilder newWord2 = new StringBuilder();
+
+//         for (int i = 0; i < word1.length; i++) newWord1.append(word1[i]);
+
+//         for (int i = 0; i < word2.length; i++) newWord2.append(word2[i]);
+
+//         return newWord1.toString().equals(newWord2.toString());
+//     }
+// }
+
+
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        
-        StringBuilder newWord1 = new StringBuilder();
-        StringBuilder newWord2 = new StringBuilder();
-
-        for (int i = 0; i < word1.length; i++) newWord1.append(word1[i]);
-
-        for (int i = 0; i < word2.length; i++) newWord2.append(word2[i]);
-
-        return newWord1.toString().equals(newWord2.toString());
+        String s1="";String s2="";
+        for(int i=0;i<word1.length;i++) s1=s1+word1[i];
+        for(int i=0;i<word2.length;i++) s2=s2+word2[i];
+        if(s1.equals(s2)) return true;
+        return false;
     }
 }
