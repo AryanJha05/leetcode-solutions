0666d6819d38452f5f13f760a182204cb002ef12
Jha Aryan <aryan42006@gmail.com>
2026-07-18 10:35:29 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (31.86%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    public static int binaryLeft(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                high = mid - 1;
+            } 
+            else if (arr[mid] < target) low = mid + 1;
+            else high = mid - 1;
+
+        }
+
+        return ans;
+    }
+
+    public static int binaryRight(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                low = mid + 1;
+            } 
+            else if (arr[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return ans;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+
+        return new int[]{binaryLeft(nums, target), binaryRight(nums,target)};
+    }
+}
