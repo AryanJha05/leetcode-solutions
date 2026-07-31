1a71c7d991280412fe951239592ec58441941b5f
Jha Aryan <aryan42006@gmail.com>
2026-07-31 09:15:16 +0530

    Time: 10 ms (66.76%) | Memory: 48.1 MB (32.08%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,4 +1,25 @@
 class Solution {
+
+    public static void countingSort(int[] arr) {
+
+        int max = 0;
+
+        for (int num : arr) max = Math.max(max, num);
+
+        int[] cnt = new int[max + 1];
+
+        for (int num : arr) cnt[num]++;
+
+        int idx = 0;
+
+        for (int i = 0; i <= max; i++) {
+            while (cnt[i] > 0) {
+                arr[idx++] = i;
+                cnt[i]--;
+            }
+        }
+    }
+
     public int minimumPushes(String word) {
         int[] freq = new int[26];
 
@@ -9,7 +30,6 @@ class Solution {
         int res = 0;
 
         for (int i = 25, idx = 0; i >= 0 && freq[i] > 0; i--, idx++) res += freq[i] * (idx / 8 + 1);
-
 
         return res;
     }
