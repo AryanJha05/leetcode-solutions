22c433779f9263c6d2980d55509d349cf43a1fbf
Jha Aryan <aryan42006@gmail.com>
2026-08-14 11:30:10 +0530

    Time: 117 ms (5.37%) | Memory: 80.8 MB (94.69%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -3,7 +3,7 @@ class Solution {
     public boolean isValid(int[] q, int pdt, int n) {
         int stores = 0;
 
-        for (int x : q) stores += (x + pdt - 1) / pdt;
+        for (int x : q) stores += Math.ceil((double)x / pdt);
 
         return stores <= n;
     }
@@ -12,8 +12,7 @@ class Solution {
         int m = quantities.length;
 
         int l = 1, r = 0;
-        for (int q : quantities)
-            r = Math.max(r, q);
+        for (int q : quantities) r = Math.max(r, q);
 
         int res = -1;
 
@@ -23,8 +22,7 @@ class Solution {
             if (isValid(quantities, mid, n)) {
                 res = mid;
                 r = mid - 1;
-            } else
-                l = mid + 1;
+            } else l = mid + 1;
         }
 
         return res;
