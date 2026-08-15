647dfd1ca51f2859498968445aee60b7c57f6cb5
Jha Aryan <aryan42006@gmail.com>
2026-08-15 16:21:06 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (46.92%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        
+        int n = citations.length;
+
+        int l = 0, r = n - 1;
+
+        while(l <= r){
+            int mid = l + (r - l)/2;
+
+            int papers = n - mid;
+
+            if(citations[mid] >= papers) r = mid - 1;
+            else l = mid + 1;
+        }
+
+        return n - l;
+    }
+}
